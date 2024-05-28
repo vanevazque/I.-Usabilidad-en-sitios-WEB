d3f66af68de61b3c310b00c9a61802feb4ccd7b6
Sebas170423 <132617418+Sebas170423@users.noreply.github.com>
2024-05-28 17:50:50 -0600

    se agrego el mapa del sitio

---
+++ mapa.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>mapa</title>
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+
+<img src="img/mapa.jpeg">
+<br><br><br><center>
+<a href="index.php" class="btn btn-info">Regresar</a>
+</center>
+</body>
+</html>
